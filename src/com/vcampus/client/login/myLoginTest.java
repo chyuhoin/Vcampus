1c8807823d6a77ac470c0d89690c498c6a0c55b2
Jasmine010202 <hy221128@163.com>
2022-08-15 16:03:22 +0800

    登录窗口

---
+++ src/com/vcampus/client/login/myLoginTest.java
@@ -0,0 +1,35 @@
+/** ===================================================
+ * Title: myLoginTest.java
+ * Created: [2022-8-14 19:56:42] by  韩宇 张星喆
+ *=====================================================
+ * Copyright:  Copyright (c)　东南大学计算机学院, 2021-2022
+ * =====================================================
+ * Description: 创建登录界面
+ *=====================================================
+ *Revised Hisytory:
+ *1. 2022-8-14,创建此文件
+ *2. 对这个文件的修改历史进行详细描述，一般包括时间，修改者，
+ *    修改的内容描述，修改的原因
+ */
+
+package com.vcampus.client.login;
+
+import javax.swing.*;
+
+public class myLoginTest {
+    public static void main(String[] args)
+    {
+        //创建登录自己的窗口对象
+        JFrame frame = new myLoginFrame("登录");
+
+        // 当关闭窗口时，退出整个程序 (在Swing高级篇教程中会介绍)
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // 设置窗口的其他参数，如窗口大小
+        frame.setSize(350, 270);
+        frame.setResizable(false);//窗口大小不可改
+
+        // 显示窗口
+        frame.setVisible(true);
+    }
+}
