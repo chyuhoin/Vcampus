cbb29fde93b894d89902cbec7b97c71c6fb8902c
liuqi <1210777906@qq.com>
2022-08-17 22:25:28 +0800

    修改了UserDao层的错误，但service和controller仍存在错误

--- src/com/vcampus/dao/UserDao.java
+++ src/com/vcampus/dao/UserDao.java
@@ -7,28 +7,28 @@ import org.junit.Test;
 import java.util.List;
 import java.util.Map;
 
-public class UserDao {
+public class UserDao extends BaseDao{
     public static String register(String username, String password,Integer type) throws Exception {
         String sql = "select * from tb_USR where studentID = '" + username + "'";
-        List<Map<String, Object>> list = CRUD.Query(sql);
+        List<Map<String, Object>> list = CRUD.Query(sql,conn);
         if (!list.isEmpty()) {
-            return "���˻��ѱ�ע��";
+            return "existed user";
         } else {
             String sql2 = "insert into tb_USR (studentID,password,type) values ('" + username + "','" + password + "',"+String.valueOf(type)+")";
-            CRUD.update(sql2);
-            return "ע��ɹ�";
+            CRUD.update(sql2,conn);
+            return "succeeded";
         }
     }
 
     public static String loginCheck(String username, String password,Integer type) throws Exception {
         String sql = "select * from tb_USR where studentID = '" + username + "' and password = '" + password + "'";
-        List<Map<String, Object>> list = CRUD.Query(sql);
+        List<Map<String, Object>> list = CRUD.Query(sql,conn);
         if (list.isEmpty()) {
-            return "�û������������";
+            return "no user";
         } else if (type !=(Integer)list.get(0).get("type")) {
-            return "���ʹ���";
+            return "wrong type";
         }else
-            return "��½�ɹ�";
+            return "succeeded";
     }
     //删除一个用户，管理员可操作
     public static Boolean deleteUser(String studentID){
