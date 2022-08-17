70048803230925d1b5b716246bed57ff34ed1e37
ietoto <ietoto@163.com>
2022-08-17 08:45:54 +0800

    加了学生管理部分

--- src/com/vcampus/server/controller/StudentController.java
+++ src/com/vcampus/server/controller/StudentController.java
@@ -2,11 +2,9 @@ package com.vcampus.server.controller;
 
 import com.vcampus.net.Message;
 import com.vcampus.pojo.Student;
-import com.vcampus.pojo.User;
 import com.vcampus.server.service.Service;
 import com.vcampus.server.service.StudentService;
 import com.google.gson.*;
-import com.vcampus.server.service.UserService;
 
 import java.util.HashMap;
 
@@ -14,25 +12,27 @@ public class StudentController implements Controller {
 
     @Override
     public Message check(Message msg){
+        StudentService service = new StudentService();
+        Gson gson = new Gson();
         switch (msg.getOperation()) {
-            case "post"://添加学生信息
-                StudentService service1 = new StudentService();
-                Gson gson1 = new Gson();
-                Student user = gson1.fromJson(msg.getData(), Student.class);
-                if(service1.addstudent(user)) return new Message("200", "{res: 'OK'}");
+            case "post"://添加学生信息 先删除对应studentID的学生，再添加新的信息
+                Student user = gson.fromJson(msg.getData(), Student.class);
+                if(!service.deleteStudent(user.getStudentID())) return new Message("200", "{res: 'NO'}");
+                if(service.addStudent(user)) return new Message("200", "{res: 'OK'}");
                 else return new Message("200", "{res: 'NO'}");
             case "get"://显示所有学生信息
-                StudentService service2 = new StudentService();
-                Gson gson2 = new Gson();
                 HashMap<String, Object> map2 = new HashMap<>();
-                map2.put("res", service2.viewAllStudents());
-                return new Message("200", gson2.toJson(map2));
-            case "getone"://显示某一指标的学生----还没写TAT
-                StudentService service3 = new StudentService();
-                Gson gson3 = new Gson();
+                map2.put("res", service.viewAllStudents());
+                return new Message("200", gson.toJson(map2));
+            case "getone"://显示某一指标的学生 如：“姓名:于济源” “指标”+“:”+“数据”
+                String data = msg.getData();
+                String[] datanew = data.split(":",2);//根据:切分字符串;切两份
                 HashMap<String, Object> map3 = new HashMap<>();
-                map3.put("res", service3.viewAllStudents());
-                return new Message("200", gson3.toJson(map3));
+                map3.put("res", service.viewSpecificStudents(datanew[0],datanew[1]));
+                return new Message("200", gson.toJson(map3));
+            case "delete"://输入学生ID，删除对应学生
+                if(service.deleteStudent(msg.getData())) return new Message("200", "{res: 'OK'}");
+                else return new Message("200", "{res: 'NO'}");
             default:
                 return new Message("404", "{res: 'Wrong Request!'}");
         }
--- src/com/vcampus/server/service/StudentService.java
+++ src/com/vcampus/server/service/StudentService.java
@@ -6,7 +6,7 @@ import com.vcampus.pojo.Student;
 import java.util.List;
 
 public class StudentService implements Service{
-    public boolean addstudent(Student user) {
+    public boolean addStudent(Student user) {
         boolean res;
         try {
             res = StudentDao.addStudent(user);
@@ -22,6 +22,25 @@ public class StudentService implements Service{
             res = StudentDao.search();
         } catch (Exception e) {
             e.printStackTrace();
+        }
+        return res;
+    }
+    public List<Student> viewSpecificStudents(String field,Object value) {
+        List<Student> res = null;
+        try {
+            res = StudentDao.search(field, value);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return res;
+    }
+    public boolean deleteStudent(String studentID) {
+        boolean res;
+        try {
+            res = StudentDao.deleteStudent(studentID);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
         }
         return res;
     }
